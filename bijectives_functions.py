ce81b6f68ec525f01a1afc8ca729d3d8263145c1
ayasgo <1ayasgo@gmail.com>
2024-02-05 16:04:25 +0100

    v1

---
+++ bijectives_functions.py
@@ -0,0 +1,134 @@
+# Il y a 30 func
+import numpy as np
+
+# Fonction identité
+def identity(x):
+    return x
+
+# Fonction exponentielle
+def exponential(x):
+    return np.exp(x)
+
+# Fonction logarithme naturel
+def logarithm(x):
+    return np.log(x)
+
+# Fonction racine carrée
+def square_root(x):
+    return np.sqrt(x)
+
+# Fonction cube
+def cube(x):
+    return x**3
+
+# Fonction inverse
+def inverse(x):
+    return 1 / x
+
+# Fonction sinus
+def sinus(x):
+    return np.sin(x)
+
+# Fonction tangente hyperbolique
+def tanh(x):
+    return np.tanh(x)
+
+# Fonction arc tangente
+def arctan(x):
+    return np.arctan(x)
+
+# Fonction puissance avec exposant fractionnaire
+def power_fractional(x):
+    return x**(1/3)
+
+# Fonction cosinus hyperbolique
+def cosh(x):
+    return np.cosh(x)
+
+# Fonction arc sinus
+def arcsin(x):
+    return np.arcsin(x)
+
+# Fonction carrée de la tangente hyperbolique
+def tanh_squared(x):
+    return np.tanh(x)**2
+
+# Fonction exponentielle de la puissance
+def exp_power(x):
+    return np.exp(x**2)
+
+# Fonction tangente hyperbolique de la racine carrée
+def tanh_sqrt(x):
+    return np.tanh(np.sqrt(x))
+
+# Fonction sinus hyperbolique
+def sinh(x):
+    return np.sinh(x)
+
+# Fonction tangente
+def tan(x):
+    return np.tan(x)
+
+# Fonction cosinus de la puissance
+def cos_power(x):
+    return np.cos(x**2)
+
+# Fonction logarithme de la tangente hyperbolique
+def log_tanh(x):
+    return np.log(np.tanh(x))
+
+# Fonction arccosinus
+def arccos(x):
+    return np.arccos(x)
+
+# Fonction carrée de la sinus hyperbolique
+def sinh_squared(x):
+    return np.sinh(x)**2
+
+# Fonction cube de la racine carrée
+def cube_sqrt(x):
+    return np.sqrt(x)**3
+
+# Fonction sinus de la racine carrée
+def sin_sqrt(x):
+    return np.sin(np.sqrt(x))
+
+# Fonction exponentielle de la tangente hyperbolique
+def exp_tanh(x):
+    return np.exp(np.tanh(x))
+
+# Fonction arctangente de la sinus hyperbolique
+def arctan_sinh(x):
+    return np.arctan(np.sinh(x))
+
+# Fonction puissance de la tangente hyperbolique
+def power_tanh(x):
+    return np.tanh(x)**3
+
+# Fonction sinus de la puissance
+def sin_power(x):
+    return np.sin(x**2)
+
+# Fonction carrée de la tangente
+def tan_squared(x):
+    return np.tan(x)**2
+
+# Fonction exponentielle de la sinus hyperbolique
+def exp_sinh(x):
+    return np.exp(np.sinh(x))
+
+# Fonction arctangente de la puissance
+def arctan_power(x):
+    return np.arctan(x**2)
+
+# Fonction carrée de la tangente hyperbolique de la racine carrée
+def tanh_sqrt_squared(x):
+    return np.tanh(np.sqrt(x))**2
+
+# Fonction arc sinus de la tangente hyperbolique
+def arcsin_tanh(x):
+    return np.arcsin(np.tanh(x))
+
+# Fonction carrée de la racine carrée
+def sqrt_squared(x):
+    return np.sqrt(np.sqrt(x))
---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+from bijectives_functions import * 
+
+SEED = 1961
+ALPHA = [ chr(ord('a') + i) for i in range(26) ]
+A, B = 10, 555
+a,b= 0.5, 8
+ASKII = []
+
+random.seed(SEED)
+MAX_DECALAGE = random.uniform(a, b)
+
+for a in ALPHA:
+    while True:
+        code = random.uniform(A, B)
+        
+        min_difference = float('inf') if not ASKII else min( c - code for c in ASKII )
+        if min_difference < MAX_DECALAGE:
+            continue
+
+        ASKII[a] : code
+    
+
+
+
+
+# Liste des fonctions
+BIJECTIVES_FUNCTIONS = [identity, exponential, logarithm, square_root, cube, inverse, sinus, tanh, arctan,
+                       power_fractional, cosh, arcsin, tanh_squared, exp_power, tanh_sqrt, sinh, tan, cos_power,
+                       log_tanh, arccos, sinh_squared, cube_sqrt, sin_sqrt, exp_tanh, arctan_sinh, power_tanh,
+                       sin_power, tan_squared, exp_sinh, arctan_power, tanh_sqrt_squared, arcsin_tanh, sqrt_squared]
+
+
+
+def decoder():
+    
+    
+
+
+
